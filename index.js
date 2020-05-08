5ddcf6035b0a42fd93081d98386bbb281b9eb2c8
Ryan Gibbons <ryan.david.gibbons@gmail.com>
2020-05-08 13:13:51 -0400

    Updated token for RTM

--- index.js
+++ index.js
@@ -3,3 +3,24 @@ const axios = require('axios')
 const dotenv = require('dotenv')
 
 dotenv.config()
+
+const bot = new SlackBot({
+    token: `${process.env.BOT_TOKEN}`,
+    name: 'bbot'
+})
+
+bot.on('start', () => {
+    const params = {
+        icon_emoji: ':alien:'
+    }
+
+    bot.postMessageToChannel(
+        'implementations',
+        'Let @bbot help you bwell!'
+
+    );
+})
+
+bot.on('error', (err) => {
+    console.log(err);
+})
